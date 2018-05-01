7ac17d7e8802992c619deea96e343fe82fb1f055
Greg kearns <kernzerly@me.com>
2018-04-30 21:29:06 -0700

    nan

--- js/app.js
+++ js/app.js
@@ -1,64 +1,26 @@
-// let item1 = "Detergent";
-// let itme2 = "cheese";
-// let item3 = "crackers";
-// let item4 = "wine";
-//
-// shoppinglist.push
+function sqrt(num) {
+  if(!isNaN(convertedNum))
+  return alert (convertedNum) * num
 
-// let favoriteMovie = ['star wars']
+} else {
+  alert (NaMN);
+  let newAnswer = prompt ("Give me something else");
+  sqrt(newAnswer);
 
-
-var favoriteMovie = ['Star Wars VI', '1983'];
-var secondFavorite = ['Return of the King', '2003'];
-var thirdFavorite = ['Equilibrium', '2002'];
-var myFavoriteMovies = [];
-myFavoriteMovies.push(favoriteMovie, secondFavorite, thirdFavorite);
-console.log(myFavoriteMovies[0][1], myFavoriteMovies[1][1], myFavoriteMovies[2][1])
-
-let i = 0;
-while (i , myFavoriteMovies.length)
-if(myFavoriteMovies[1][0]) === 'Star Wars';
-let correct entry
-console.log(i);
-
-console.log(myFavoriteMovies[])
-i++;
 }
+function alertString(str)
+if(str.charAt(str.length - 1))
+str += '.';
+}
+str = str.charAt(0).toUpper(
+  alert(str);
+)
+// let myStr = prompt('Hey,
 
-var arr = ['this is the first element', 'this is the second element', 'this is the last element'];
-console.log(arr[0]);              // logs 'this is the first element'
-console.log(arr[1]);              // logs 'this is the second element'
-console.log(arr[arr.length - 1]); // logs 'this is the last element'
+stringToFlip("").reverse().
 
-if(myFavoriteMOvies[1][0] ==='star wars'
 
-// //Result:
-//
-//
-// // var movieYears = [];
-// // movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1],myFavoriteMovies[2][1]);
-// // alert(movieYears);
-// //
-// // <h1>Array.push() - Return Value</h1>
-// //
-// // <p>The push() method adds new items to the end of an array.</p>
-// //
-// // <p>This example demonstrates how the push() method returns the length of the array, after the insertion.</p>
-// //
-// // <button onclick="myFunction()">Alert the length of the array</button>
-// //
-// // <p>The array:</p>
-// <p id="demo"></p>
-//
-// <script>
-// var movies = ["the good the bad and the ugly", "grape of wrath", "matrix", "dirty harry"];
-// document.getElementById("demo").innerHTML = movies;
-//
-// function myFunction() {
-//   var x = movies.push("its a mad mad mad mad world");
-//   alert(x);
-//   document.getElementById("demo").innerHTML = movies;
-// }
-// </script>
-//
-// </body>
+function definePal(string)
+let reverse =
+ string.split('').reversed().
+}
