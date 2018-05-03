f639226483df43af4e390313210c1a26feb2862e
Greg kearns <kernzerly@me.com>
2018-05-02 20:16:10 -0700

    day 8 fizz buzz

---
+++ js/app.js
@@ -0,0 +1,35 @@
+<script>
+function myFunction() {
+    var text;
+    var numbers = document.getElementById("myInput").value;
+
+    switch(numbers 1-100) {
+        case "numbers"
+            text = "numbers 1-100";
+        break;
+        case "":
+        text = "I am not a fan of orange.";
+        break;
+        case "Apple":
+        text = "How you like them apples?";
+        break;
+        default:
+        text = "I have never heard of that fruit...";
+    }
+    document.getElementById("demo").innerHTML = text;
+
+    const  body = document.queryStatement
+    for(let i = 1; <= 100; 1++);
+    let p = document.createElement
+    if(i % 3 ===0) && i % 5 =
+    p.textContent = 'Buzz';
+  } else {
+    p.textContent = i;
+
+  }
+  body.appendChild(p);
+}
+</script>
+
+</body>
+</html>
